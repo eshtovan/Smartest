54788975bb6c36f22f06b6eb05371fdedc1d416c
yansvai <yansvai@gmail.com>
2019-03-12 11:04:38 +0200

    Gamemanager

--- Smartest/Unity/SmartTest/Assets/_Scripts/GameManager.cs
+++ Smartest/Unity/SmartTest/Assets/_Scripts/GameManager.cs
@@ -1,20 +1,52 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum SensorType
+{
+    Velodyne,
+    Novotel,
+    Tiltan
+
+}
+public class Sensor
+{
+    public int SensorID;
+    public SensorType SensorType;
+    public Vector3 SensorLocation;
+    public GameObject Go;
+
+}
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Sensor[] sensors;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    
+
+    public void CreateSensor(SensorType sensorType)
+    {
+        GameObject parent = GameObject.FindGameObjectWithTag("Car");
+        foreach (var sen in sensors)
+        {
+            if (sen.SensorType == sensorType)
+            {
+              
+                GameObject newSensor = Instantiate(sen.Go,parent.transform);
+            }
+        }
+
+    }
+
+
 }
