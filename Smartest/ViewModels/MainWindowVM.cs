5fbba6e6aac68de1da26aa122e1a6a4436066185
Eshtovan <lirant13@walla.co.il>
2019-03-24 15:45:00 +0200

    Added Unity embaded EXE file inside the wpf

--- Smartest/ViewModels/MainWindowVM.cs
+++ Smartest/ViewModels/MainWindowVM.cs
@@ -1,7 +1,13 @@
-﻿using Smartest.Infrastructure.Interfaces;
+﻿using System;
+using Smartest.Infrastructure.Interfaces;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Input;
 using GalaSoft.MvvmLight; 
 using CommonServiceLocator;
+using GalaSoft.MvvmLight.Command;
 using Smartest.ViewModels.VehicleConfigurationVM;
 using Smartest.Utilities;
 
@@ -16,18 +22,64 @@ namespace Smartest.ViewModels
         //    set { tabViewModels = value; }
         //}
 
+        public IntPtr MainWindowHandle { get; set; }
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern long SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        public ICommand SaveButton { get; }
+
         //internal ITabViewModel SelectedTabViewModel;
         public MainWindowVm()
         {
             ProjectsData.CurrentProjectName = "Hummer";
 
             ProjectsData.CurrentConfigurationName = "Hummer First Test";
+
+            SaveButton = new RelayCommand(SaveConfigurationCommand);
+
+            var path = @"C:\Program Files\Notepad++\notepad++.exe";
+           // LoadUnityProcess(path);
+
             //TabViewModels = new ObservableCollection<ITabViewModel>();
             //TabViewModels.Add(new SensorViewModel { Header = "Tab B" });
             ////TabViewModels.Add(new ViewModelB { Header = "Tab B" });
             ////TabViewModels.Add(new ViewModelC { Header = "Tab C" });
 
             //SelectedTabViewModel = TabViewModels[0];
+        }
+
+        private object LoadUnityProcess//string path
+        {
+            get
+            {
+                //https://answers.unity.com/questions/245242/is-it-possible-to-use-unity-inside-wpf.html
+                //https://social.msdn.microsoft.com/Forums/vstudio/en-US/a08de79c-f16a-4d10-ab7f-5d0a99a91ff5/external-exe-inside-wpf-window?forum=wpf
+
+
+                //  WindowsFormsHost host = new WindowsFormsHost();
+                //  System.Windows.Forms.Panel p = new System.Windows.Forms.Panel();
+                //   host.Child = p;
+                //   nPage.Header = "nouveau";
+                //   nPage.Content = host;
+                //   TabPage.Items.Add(nPage);
+                Process proc = Process.Start(
+                    new ProcessStartInfo()
+                    {
+                        FileName = @"C:\Program Files\Notepad++\notepad++.exe",
+
+                        WindowStyle = ProcessWindowStyle.Normal
+                    });
+                Thread.Sleep(1000);
+                SetParent(proc.MainWindowHandle, this.MainWindowHandle);
+
+                return proc;
+
+            }
+        }
+
+        private void SaveConfigurationCommand()
+        {
+             
         }
 
 
@@ -103,6 +155,9 @@ namespace Smartest.ViewModels
                 return ServiceLocator.Current.GetInstance<ConfigurationViewModel>();
             }
         }
-    
+
+        // https://stackoverflow.com/questions/19000721/running-an-exe-file-in-a-new-itemtab-in-wpf-application
+        // 
+        
     }
 }
---
+++ Smartest/Views/UnityWindow.xaml.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms; 
+using UserControl = System.Windows.Controls.UserControl;
+
+namespace Smartest.Views
+{
+    /// <summary>
+    /// Interaction logic for UnityWindow.xaml
+    /// </summary>
+    public partial class UnityWindow : UserControl
+    {
+        [DllImport("user32.dll", EntryPoint = "GetWindowThreadProcessId", SetLastError = true,
+            CharSet = CharSet.Unicode, ExactSpelling = true,
+            CallingConvention = CallingConvention.StdCall)]
+        private static extern long GetWindowThreadProcessId(long hWnd, long lpdwProcessId);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern long SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowLongA", SetLastError = true)]
+        private static extern long GetWindowLong(IntPtr hwnd, int nIndex);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLongA", SetLastError = true)]
+        public static extern int SetWindowLongA([System.Runtime.InteropServices.InAttribute()]
+            System.IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern long SetWindowPos(IntPtr hwnd, long hWndInsertAfter, long x, long y, long cx, long cy,
+            long wFlags);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool MoveWindow(IntPtr hwnd, int x, int y, int cx, int cy, bool repaint);
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hwnd, int nCmdShow);
+
+        private const int SWP_NOOWNERZORDER = 0x200;
+        private const int SWP_NOREDRAW = 0x8;
+        private const int SWP_NOZORDER = 0x4;
+        private const int SWP_SHOWWINDOW = 0x0040;
+        private const int WS_EX_MDICHILD = 0x40;
+        private const int SWP_FRAMECHANGED = 0x20;
+        private const int SWP_NOACTIVATE = 0x10;
+        private const int SWP_ASYNCWINDOWPOS = 0x4000;
+        private const int SWP_NOMOVE = 0x2;
+        private const int SWP_NOSIZE = 0x1;
+        private const int GWL_STYLE = (-16);
+        private const int WS_VISIBLE = 0x10000000;
+        private const int WS_CHILD = 0x40000000;
+
+
+        //[System.Runtime.InteropServices.StructLayoutAttribute(System.Runtime.InteropServices.LayoutKind.Sequential)]
+        //public struct HWND__
+        //{
+
+        //    /// int
+        //    public int unused;
+        //}
+
+
+
+        System.Diagnostics.Process _cmdProcess;
+
+        readonly System.Windows.Forms.Panel _panel = new System.Windows.Forms.Panel();
+
+
+        ~UnityWindow()
+        {
+            this.Dispose();
+        }
+
+
+        public UnityWindow()
+        {
+            InitializeComponent();
+            this.Loaded += OnVisibleChanged;
+        }
+
+
+        public void Dispose()
+        {
+            if (_cmdProcess != null)
+            {
+                _cmdProcess.Close();
+                _cmdProcess.Dispose();
+            }
+            // this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Create control when visibility changes
+        /// </summary>
+        /// <param name="e">Not used</param>
+        protected void OnVisibleChanged(object s, RoutedEventArgs e)
+        {
+
+
+            /*
+           //                  https://carldesouza.com/console-app-wpf-winapi-uii/
+            //https://stackoverflow.com/questions/6837330/make-process-window-visible-invisble-in-net
+
+            */
+
+            var processStartInfo = new System.Diagnostics.ProcessStartInfo(@"C:\OLD_PC\ConvoyUnity-master\Builds\Convoy.exe")
+                {
+                   // WindowStyle = ProcessWindowStyle.Hidden
+                };
+            
+
+            _cmdProcess = Process.Start(processStartInfo);
+             
+            if (_cmdProcess != null)
+            {
+                _cmdProcess.WaitForInputIdle();
+
+                Thread.Sleep(500);
+                //_panel.AutoSize = true;
+                _panel.Dock = DockStyle.Fill;
+                // Use Win32API to set the command process to the panel
+                SetParent(_cmdProcess.MainWindowHandle, _panel.Handle);
+                WindowsFormsHost.Child = _panel;
+
+                ShowWindow(_cmdProcess.MainWindowHandle, 5);
+                //            // Move the window to overlay it on this window
+                //            MoveWindow(_appWin, 0, 0, (int)this.ActualWidth, (int)this.ActualHeight, true);
+
+            }
+        }
+    }
+}
